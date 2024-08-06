f230005ce0c5c5b5cdb0504ab3e1f6099bb4673c
Abobus2234 <134945612+Abobus2234@users.noreply.github.com>
2024-08-06 12:05:29 +0200

    Update environment.prod.ts

--- Web/src/environments/environment.prod.ts
+++ Web/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://brbad-web-api-balancer-545357988.eu-west-3.elb.amazonaws.com:3000/api'
+    apiUrl: 'http://xxx.eu-west-3.elb.amazonaws.com:3000/api'
 }
